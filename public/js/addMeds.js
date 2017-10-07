082467a36d601707a9d36a37c2724d3d32d58c1c
Alfredo Rodriguez <alrod909@gmail.com>
2017-10-07 11:38:10 -0600

    Some script changes

--- public/js/addMeds.js
+++ public/js/addMeds.js
@@ -25,15 +25,6 @@ $( document ).ready(function() {
                 console.log(data);
             });
 
-        $("#med_name2").val("");
-        $("#drug_class2").val("");
-        $("#med_desc2").val("");
-        $("#dosage2").val("");
-        $("#frequency2").val("");
-        $("#quantity2").val("");
-        $("#doctor2").val("");
-        $("#doctor_number2").val("");
-
     });
 
 
